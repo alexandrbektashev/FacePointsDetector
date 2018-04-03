2427d38d3019eafb2ab6a1314384135c15e3a64c
alexandrbektashev <alexandr.bektashev@gmail.com>
2018-04-03 22:34:40 +0300

    emotion detection is done

---
+++ AffdexSampleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Affdex;
+
+namespace AffdexSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Affdex.Detector myDetector = new Affdex.CameraDetector(0, 30, 10, 1, Affdex.FaceDetectorMode.LARGE_FACES);
+
+            myDetector.setClassifierPath("C:\\Program Files\\Affectiva\\AffdexSDK\\data");
+
+            ProcessCameraFeed feedform = new ProcessCameraFeed(myDetector);
+            myDetector.setDetectAllEmotions(true);
+            myDetector.setDetectSmirk(true);
+            myDetector.setDetectGlasses(true);
+
+            myDetector.start();
+            feedform.ShowDialog();
+            myDetector.stop();
+            
+
+        }
+
+    }
+}
