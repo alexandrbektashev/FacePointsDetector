ee153d6a2e8033dab2e7efaacd92ba4dd53e1580
alexandrbektashev <alexandr.bektashev@gmail.com>
2018-04-25 18:41:38 +0300

    empty analysers

---
+++ ProcessVideoFile/ProcessVideoFile/Analyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessVideoFile
+{
+    class Analyser
+    {
+
+        //in this class I'm going to make some methods which would give information
+        //input info - timestamp
+        //output yes\no, confidence
+
+        //I should also make a face model which will understand eyes position
+
+
+
+    }
+}
